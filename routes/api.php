25ae6c0fe63eac0db03e95f3620cbbda211d0ba6
IlhamShiddiq <shdqillham123@gmail.com>
2025-01-22 10:02:21 +0700

    fix: remove unused resource

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\OutletController;
 use Illuminate\Support\Facades\Route;
-
-Route::prefix('auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-});
 
 Route::prefix('brands')->group(function () {
     Route::get('/', [BrandController::class, 'list']);
